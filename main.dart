3463ff5b077a10796ebd66fc3bcb837ffe637e93
kareem Abd Alatef Mostafa <159265411+kareeem959@users.noreply.github.com>
2024-02-15 20:52:16 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,11 +1,35 @@
 import 'dart:io';
 
 void main() {
-  print("enter your name");
-  String? name;
-  name = stdin.readLineSync();
-  print("hello${name}");
-  print("enter your age ");
-  int age=int.parse(stdin.readLineSync()!);
-  print("name ${name}\n age ${age}  ");
+    String name, country, selectedDapratment = "";
+  double age;
+  int department;
+  print("enter name : ");
+  name = stdin.readLineSync()!;
+  print("enter age : ");
+  age = double.parse(stdin.readLineSync()!);
+  print("enter country : ");
+  country = stdin.readLineSync()!;
+  print("select your section 1- Soft \n2- Bio \n3- General )");
+  department = int.parse(stdin.readLineSync()!);
+  switch (department) {
+    case 1:
+      print("your department 14000");
+      selectedDapratment = "soft";
+    case 2:
+      print("your department 14000");
+      selectedDapratment = "Bio";
+      break;
+    case 3:
+      selectedDapratment = "general";
+      print("your department 1000");
+      break;
+    default:
+      print("select your section 1- Soft \n2- Bio \n3- General )");
+      break;
+  }
+  print("=================== your infromation ===========");
+  print(
+      "name : $name ,age : $age ,countrye : $country , department : $selectedDapratment");
+
 }
